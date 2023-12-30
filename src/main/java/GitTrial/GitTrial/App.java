c30a12de5724242755c2d9364e7f9a66008f858f
Abderrahmen <Abdouattia26@gmail.com>
2023-12-29 20:17:42 -0500

    Trial git Commit To resolve isssue

---
+++ src/main/java/GitTrial/GitTrial/App.java
@@ -0,0 +1,14 @@
+package GitTrial.GitTrial;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("My name is Adam Attia");
+    }
+}
