975939821daa58331ef92500011e451f36b560bf
Abderrahmen <Abdouattia26@gmail.com>
2023-12-31 16:19:13 -0500

    Adding New Method

--- src/main/java/GitTrial/GitTrial/App.java
+++ src/main/java/GitTrial/GitTrial/App.java
@@ -1,5 +1,7 @@
 package GitTrial.GitTrial;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -10,5 +12,14 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("My name is Adam Attia");
+        
+        getName();
+    }
+    public static void getName() {
+    	Scanner scanner = new Scanner(System.in);
+    	System.out.println("Enter the nAme: ");
+    	
+    	String nameString = scanner.nextLine();
+    	System.out.println("The name was enetered is : " + nameString);
     }
 }
